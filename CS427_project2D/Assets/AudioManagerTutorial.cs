caeb83fdebc06ff2e463997c7cc9934fbbbec893
ntqduy2908 <56163606+ntqduy2908@users.noreply.github.com>
2021-07-12 22:24:25 +0700

    VolumeUpdate

---
+++ CS427_project2D/Assets/AudioManagerTutorial.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class AudioManagerTutorial : MonoBehaviour
+{
+    private static readonly string FirstPlay = "FirstPlay";
+    private static readonly string BackgroundPref= "BackgroundPref";
+    private int firstPlayInt;
+    public Slider backgroundSlider;
+    private float backgroundFloat;
+    public AudioSource backgroundAudio;
+    void Start()
+    {
+        firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+        if(firstPlayInt==0)
+        {
+            backgroundFloat = .125f;
+            backgroundSlider.value = backgroundFloat;
+            PlayerPrefs.SetFloat(BackgroundPref, backgroundFloat);
+            PlayerPrefs.SetInt(FirstPlay, -1);
+        }   
+        else
+        {
+            backgroundFloat = PlayerPrefs.GetFloat(BackgroundPref);
+            backgroundSlider.value = backgroundFloat;
+        }    
+    }
+    public void SaveSoundSettings()
+    {
+        PlayerPrefs.SetFloat(BackgroundPref, backgroundSlider.value);
+    }
+    void OnApplicationFocus(bool inFocus)
+    {
+        if(!inFocus)
+        {
+            SaveSoundSettings();
+        }    
+    }
+    public void UpdateSound()
+    {
+        backgroundAudio.volume = backgroundSlider.value;
+        /*for(int i=0;i<backgroundAudio.Length;i++)
+        {
+            backgroundAudio[i].volume = backgroundSlider.value;
+        }    */
+    }    
+}
